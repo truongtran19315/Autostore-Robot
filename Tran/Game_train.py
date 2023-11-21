05cb3959446c25db15aecac425f1a646f0b660d0
truongtran19315 <truong.chiptttran@gmail.com>
2023-11-21 16:40:45 +0700

    train

--- Tran/Game_train.py
+++ Tran/Game_train.py
@@ -18,9 +18,9 @@ import cv2
 
 videoFile_path = getlogVideo_path(videoFolderPath)
 recordVideo = cv2.VideoWriter(videoFile_path,
-                            cv2.VideoWriter_fourcc(*'XVID'),
-                            10,
-                            (GAME_SETTING.SCREEN_WIDTH, GAME_SETTING.SCREEN_HEIGHT))
+                              cv2.VideoWriter_fourcc(*'XVID'),
+                              10,
+                              (GAME_SETTING.SCREEN_WIDTH, GAME_SETTING.SCREEN_HEIGHT))
 Video = 1
 
 q_table_filename = "q_table.pkl"
@@ -40,7 +40,8 @@ if os.path.exists(q_table_path):
         q_table = pickle.load(f)
     print("Q-table loaded successfully!")
 else:
-    q_table = np.random.uniform(low=-1, high=1, size=(*game.new_observation_shape, ACTION_SPACE))
+    q_table = np.random.uniform(
+        low=-1, high=1, size=(*game.new_observation_shape, ACTION_SPACE))
     print("No existing Q-table found. Creating a new Q-table ... ")
 
 
@@ -58,29 +59,32 @@ all_States = np.zeros(game.new_observation_shape)
 for i in range(n_epsilondes):
     print(f"Epsilon {i}")
     game.reset()
-    
+
     trackPos = Env.copy()
-    
+
     log_path = getLog_path(logFolderPath)
     with open(log_path, 'a') as file:
-        print(f'\n******************** Epsilon {i} ***********************', file=file)
-    
-    reward, _, trackPosition, done = game.run_episode(q_table, Video, trackPos, log_path, all_States)
+        print(
+            f'\n******************** Epsilon {i} ***********************', file=file)
+
+    reward, _, trackPosition, done = game.run_episode(
+        q_table, Video, trackPos, log_path, all_States)
     if done == PLAYER_SETTING.GOAL:
         goal_count += 1
     reward_records.append(reward)
     goal_count_str = 'goal counter: ' + str(goal_count)
-    cv2.putText(trackPosition, goal_count_str, (50, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, COLOR.WHITE, 1)
+    cv2.putText(trackPosition, goal_count_str, (50, 350),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, COLOR.WHITE, 1)
     trackPosition_path = getlogPosition_path(imageFolderPath, done)
     cv2.imwrite(trackPosition_path, trackPosition)
-    recordVideo.write(trackPosition)    
-    # TODO update diagram
-    if (i % 10 == 0):
-        game.creat_axes(axes, i)
-        # plt.draw()
-        # plt.pause(1)
-        plt.savefig(diagram_path)
-        # print(f"Diagram saved to: {diagram_path}")
+    recordVideo.write(trackPosition)
+    # # TODO update diagram
+    # if (i % 10 == 0):
+    #     game.creat_axes(axes, i)
+    #     # plt.draw()
+    #     # plt.pause(1)
+    #     plt.savefig(diagram_path)
+    #     # print(f"Diagram saved to: {diagram_path}")
 
     with open(q_table_path, "wb") as f:
         pickle.dump(q_table, f)
@@ -88,7 +92,7 @@ for i in range(n_epsilondes):
     # print("-----------------------------------------------------")
     # for j in range(len(screenRecord)):
     #     recordVideo.write(screenRecord[j])
-        
+
     if cv2.waitKey(1) & 0xFF == 'q':
         recordVideo.release()
         break
