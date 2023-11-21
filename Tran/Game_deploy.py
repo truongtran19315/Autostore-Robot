7656246e37bbe059783f51fcf90f01d0b3487d7b
truongtran19315 <truong.chiptttran@gmail.com>
2023-11-21 16:40:08 +0700

    update deploy

--- Tran/Game_deploy.py
+++ Tran/Game_deploy.py
@@ -1,6 +1,20 @@
 import pickle
 from consts import *
 import numpy as np
+from Game_class import *
 
-with open("C:\\Users\\truon\\PROJECTS\\PYTHON\\do-an-hk231\\DATA\\2023-11-11_V3\\q_table.pkl", "rb") as f:
-    data = pickle.load(f)
+
+with open("C:\\Users\\truon\\PROJECTS\\PYTHON\\do-an-hk231\\Autostore-Robot\\Tran\\DATA\\2023-11-18_V1\\q_table.pkl", "rb") as f:
+    q_table = pickle.load(f)
+
+screen = np.zeros((720, 1280, 3), dtype=np.uint8)
+robot = Game(screen)
+Env = robot.getEnv()
+
+state = robot.game.observe()
+while True:
+    action = np.argmax(q_table[tuple(state)])
+    print(f'Action: {action}')
+    robot.game.action(action)
+    next_state = robot.game.observe()
+    state = next_state
