7ebc58359026084f40f317474cb139fe505c1dcf
truongtran19315 <truong.chiptttran@gmail.com>
2024-03-04 17:10:13 +0700

    fix no random

--- HK232/obstacles.py
+++ HK232/obstacles.py
@@ -25,10 +25,11 @@ class StaticObstacles():
         yEnd = self.yCenter + self.height//2
         if self.shape == 'rectangle':
             cv2.rectangle(screen, (xStart, yStart),
-                            (xEnd, yEnd), COLOR.GREEN, -1)
+                          (xEnd, yEnd), COLOR.GREEN, -1)
         else:
             cv2.rectangle(screen, (xStart, yStart),
-                            (xEnd, yEnd), COLOR.RED, -1)
+                          (xEnd, yEnd), COLOR.RED, -1)
+
 
 class RectangleObstacles(StaticObstacles):
     def __init__(self, xCenter, yCenter, height, width) -> None:
@@ -55,30 +56,38 @@ class Goal():
         self.radius = GAME_SETTING.GOAL_WIDTH / 2 * math.sqrt(2)
         self.width = GAME_SETTING.GOAL_WIDTH
         self.height = GAME_SETTING.GOAL_WIDTH
-    
+
     def randomGoal(self, obstacleArr):
         rand1 = random.randint(1, 15)
         rand2 = random.randint(1, 8)
-        self.x = obstacleArr[rand1 - 1][0] + (rand2 % 2 - 1) * GAME_SETTING.GRID_WIDTH 
-        self.y = obstacleArr[rand1 - 1][1] + (rand2 % 4 - 2) * GAME_SETTING.GRID_WIDTH
+        self.x = obstacleArr[rand1 - 1][0] + \
+            (rand2 % 2 - 1) * GAME_SETTING.GRID_WIDTH
+        self.y = obstacleArr[rand1 - 1][1] + \
+            (rand2 % 4 - 2) * GAME_SETTING.GRID_WIDTH
         self.xCenter = self.x + GAME_SETTING.GOAL_WIDTH / 2
         self.yCenter = self.y + GAME_SETTING.GOAL_WIDTH / 2
 
+        #! added to no Random
+        self.x = 300
+        self.y = 200
+        self.xCenter = self.x + GAME_SETTING.GOAL_WIDTH/2
+        self.yCenter = self.y + GAME_SETTING.GOAL_WIDTH/2
+
     def draw(self, screen):
         cv2.rectangle(screen, (self.x, self.y),
-                   (self.x + GAME_SETTING.GOAL_WIDTH, self.y + GAME_SETTING.GOAL_WIDTH), COLOR.PURPLE, -1)
+                      (self.x + GAME_SETTING.GOAL_WIDTH, self.y + GAME_SETTING.GOAL_WIDTH), COLOR.PURPLE, -1)
 
 
 class Obstacles():
     def __init__(self, numOfRect, map) -> None:
-        
+
         self.goal = Goal()
 
         self.obstacles = []  # to save all obstacles
 
         self.numberOfRectangleObstacles = numOfRect
-        
-        # define wall 
+
+        # define wall
         # x, y height, width - 1280x720
         # self.wallArr = [[250, 0, 4, 500],
         #                 [0, 250, 500, 4],
@@ -87,30 +96,30 @@ class Obstacles():
         # self.wallArr = []
 
         # x, y height, width - 1280x720
-        self.rectangleObstaclesArr = [  [60, 80, 80, 40],
-                                        [140, 80, 80, 40],
-                                        [220, 80, 80, 40],
-                                        [300, 80, 80, 40],
-                                        [380, 80, 80, 40],
+        self.rectangleObstaclesArr = [[60, 80, 80, 40],
+                                      [140, 80, 80, 40],
+                                      [220, 80, 80, 40],
+                                      [300, 80, 80, 40],
+                                      [380, 80, 80, 40],
 
-                                        [60, 200, 80, 40],
-                                        [140, 200, 80, 40],
-                                        [220, 200, 80, 40],
-                                        [300, 200, 80, 40],
-                                        [380, 200, 80, 40],
+                                      [60, 200, 80, 40],
+                                      [140, 200, 80, 40],
+                                      [220, 200, 80, 40],
+                                      [300, 200, 80, 40],
+                                      [380, 200, 80, 40],
 
-                                        [60, 320, 80, 40],
-                                        [140, 320, 80, 40],
-                                        [220, 320, 80, 40],
-                                        [300, 320, 80, 40],
-                                        [380, 320, 80, 40],]
+                                      [60, 320, 80, 40],
+                                      [140, 320, 80, 40],
+                                      [220, 320, 80, 40],
+                                      [300, 320, 80, 40],
+                                      [380, 320, 80, 40],]
         # self.rectangleObstaclesArr = []
-        
+
         # add each of obstacleArr to a list of obstacle object
         self.listObstacle()
-        
+
         self.goal.randomGoal(self.rectangleObstaclesArr)
-    
+
     def listObstacle(self):
         self.rectangleObstacles = []  # to save all rectangle obstacles
         for obstacle in self.rectangleObstaclesArr:
@@ -124,7 +133,7 @@ class Obstacles():
         #     rectangle = StaticObstacles(
         #         xCenter=obstacle[0], yCenter=obstacle[1], height=obstacle[2], width=obstacle[3], shape='wall')
         #     self.wall.append(rectangle)
-        #     self.obstacles.append(rectangle)       
+        #     self.obstacles.append(rectangle)
 
     def generateObstacles(self, screen):
         for obstacle in self.obstacles:
