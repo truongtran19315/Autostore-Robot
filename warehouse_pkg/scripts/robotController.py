74c26ca9aa5c90562ca48f86c5d92f317da20d8f
truongtran19315 <truong.chiptttran@gmail.com>
2024-04-29 10:32:52 +0700

    perf: run robot every 10Hz

--- warehouse_pkg/scripts/robotController.py
+++ warehouse_pkg/scripts/robotController.py
@@ -50,17 +50,20 @@ class RobotController:
         self.current_x = PLAYER_SETTING.X_INIT_POS
         self.current_y = PLAYER_SETTING.Y_INIT_POS
         self.current_yaw_2pi = self.convert_current_yaw_2pi()
-        self.rate = rospy.Rate(10)  # Update rate 10 Hz
         self.lidar_sub = rospy.Subscriber('/scan', LaserScan, self.lidar_callback)
         self.lidars = []
         self.step_counter = 1
         self.q_table = None
+        self.load_q_table(Q_TABLE_PATH)
         
         self.angular_rotate_speed = 0.4    #! Speed rotate robot
-        self.angular_adjust_right_speed = 0.04
+        self.angular_adjust_right_speed = 0.08
         self.distance_forward = 0.18        #! move forward robot 20 cm (1 step)
         self.move_forward_speed = 0.10
         
+        # Timer with 10 Hz
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.control_loop)
+
         print("Initialization completed...!")
 
     def odom_callback(self, msg):
@@ -77,16 +80,14 @@ class RobotController:
 
     def lidar_callback(self, data):
         # Store lidar data
-        # self.lidars = [data.ranges[270], data.ranges[0], data.ranges[90], data.ranges[180]]
         lidar_indices = np.round((270 + np.arange(0, PLAYER_SETTING.CASTED_RAYS) * PLAYER_SETTING.STEP_ANGLE)) % 360
-        lidar_indices = lidar_indices.astype(int)  # Chuyển đổi thành kiểu int 
-        # self.lidars = data.ranges[lidar_indices] #! error
+        lidar_indices = lidar_indices.astype(int)  
         lidar_data = [data.ranges[i] for i in lidar_indices]
         self.lidars = np.array(lidar_data)
 
     def move_forward(self, distance):
         twist_cmd = Twist()
-        twist_cmd.linear.x = self.move_forward_speed  #! Forward speed
+        twist_cmd.linear.x = self.move_forward_speed  
         start_position = rospy.wait_for_message('/odom', Odometry).pose.pose.position
         last_yaw = self.current_yaw
         self.cmd_vel_pub.publish(twist_cmd)
@@ -96,12 +97,11 @@ class RobotController:
             distance_moved = math.sqrt((current_position.x - start_position.x)**2 + (current_position.y - start_position.y)**2)
             if distance_moved >= distance:
                 break
-            # Adjust angular speed based on current yaw error
             error_yaw = self.normalize_angle(current_yaw - last_yaw)
-            twist_cmd.angular.z = -0.1 * error_yaw  # Adjust angular speed if needed
+            twist_cmd.angular.z = -0.1 * error_yaw  
             self.cmd_vel_pub.publish(twist_cmd)
             last_yaw = current_yaw
-            self.rate.sleep()
+            rospy.sleep(0.1)
         twist_cmd.linear.x = 0
         twist_cmd.angular.z = 0
         self.cmd_vel_pub.publish(twist_cmd)
@@ -113,7 +113,7 @@ class RobotController:
         angle_moved = 0.0
         self.cmd_vel_pub.publish(twist_cmd)
         while not rospy.is_shutdown() and abs(angle_moved) < abs(angle):
-            self.rate.sleep()
+            rospy.sleep(0.1)
             delta_yaw = self.normalize_angle(self.current_yaw - last_yaw)
             angle_moved += delta_yaw
             last_yaw = self.current_yaw
@@ -123,7 +123,7 @@ class RobotController:
     def normalize_angle(self, angle):
         return (angle + math.pi) % (2 * math.pi) - math.pi
 
-    def adjust_to_right_angle(self, speed_rotate= None, target_yaw= None):
+    def adjust_to_right_angle(self, speed_rotate=None, target_yaw=None):
         if speed_rotate is None:
             speed_rotate = self.angular_adjust_right_speed
         if target_yaw is None:
@@ -170,13 +170,13 @@ class RobotController:
             self.current_x, self.current_y, PLAYER_SETTING.X_GOAL, PLAYER_SETTING.Y_GOAL)
         alpha = self.calculate_angle_difference(goal_angle)
         
+        print("current yaw = {} = {} ".format(self.current_yaw, self.convert_degree(self.current_yaw)))
         print("Current Angle : {}, Angle Goal : {}".format(self.convert_degree(self.current_yaw_2pi), self.convert_degree(goal_angle)))
         print("==> ALPHA : {} degree".format(self.convert_degree(alpha)))
         print("DISTANCE : {}".format(round(self.distance_Robot_to_Goal(), 2)))
         
         lidars = np.reshape(self.lidars, (SPACE.REGION_LIDAR_SPAGE,SPACE.SECTIONS_LIDARSPACE))
-        lidars_RegionSelected = np.min(lidars, axis=1)   # Tìm phần tử nhỏ nhất trong mỗi hàng (mỗi vùng)
-
+        lidars_RegionSelected = np.min(lidars, axis=1)   
         lidarLength_digitized = np.digitize(lidars_RegionSelected, SPACE.LIDAR_LENGTH_SEGMENT)
 
         distanceGoal_bin = np.linspace(
@@ -192,29 +192,26 @@ class RobotController:
 
         return np.concatenate((infoStateVector, lidarLength_digitized))
 
-    def run(self):
-        self.load_q_table(Q_TABLE_PATH)
-        rospy.sleep(1)
-        while not rospy.is_shutdown():
-            print("----- Step {} :".format(self.step_counter))
-            state = self.observe()
-            print("state = {}".format(state))
-            action = self.choose_action(state)
-            print("action = {}".format(action))
-            if action == ACTIONS.FORWARD:
-                self.adjust_to_right_angle(0.08)
-                self.move_forward(self.distance_forward)
-                self.stop_robot()
-            elif action == ACTIONS.TURN_RIGHT:
-                self.adjust_to_right_angle()
-                self.rotate(math.pi/2, True)
-                self.stop_robot()
-            elif action == ACTIONS.TURN_LEFT: 
-                self.adjust_to_right_angle()
-                self.rotate(math.pi/2, False)
-                self.stop_robot()
-            
-            self.step_counter += 1
+    def control_loop(self, event):
+        state = self.observe()
+        print("state = {}".format(state))
+        action = self.choose_action(state)
+        print("---------- Step {} ---------------- :".format(self.step_counter))
+        print("action = {}".format(action))
+        if action == ACTIONS.FORWARD:
+            self.adjust_to_right_angle(0.08)
+            self.move_forward(self.distance_forward)
+            self.stop_robot()
+        elif action == ACTIONS.TURN_RIGHT:
+            self.adjust_to_right_angle()
+            self.rotate(math.pi/2, True)
+            self.stop_robot()
+        elif action == ACTIONS.TURN_LEFT: 
+            self.adjust_to_right_angle()
+            self.rotate(math.pi/2, False)
+            self.stop_robot()
+        
+        self.step_counter += 1
 
     def angleBetweenTwoPoints(self, xPointA, yPointA, xPointB, yPointB):
         delta_x = xPointB - xPointA
@@ -239,11 +236,9 @@ class RobotController:
     def convert_degree(self, angle):
         return round(angle * 180 / math.pi, 2)
 
-
-
 if __name__ == '__main__':
     try:
         controller = RobotController()
-        controller.run()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
